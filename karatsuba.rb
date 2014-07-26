ab9ee47239d6adcf4452e5c581ff56be0fa52547
Asjed (Azi) Hussain <asjed.hussain@gmail.com>
2014-07-26 16:45:53 -0400

    Refactor Karatsuba method)

--- karatsuba.rb
+++ karatsuba.rb
@@ -1,30 +1,31 @@
-#only works with two numbers of same length
-require 'pry'
+#there's a bug in this... doesn't work for some numbers
 
 def karatsuba(x,y)
-  if x < 10 && y < 10
+  if x < 10 || y < 10
     return x * y
   else
     x_arr = x.to_s.split(//)
     y_arr = y.to_s.split(//)
-    n = x_arr.length #since x and y are same length
 
-    a = x_arr.slice(0, ( n / 2.to_f).ceil)
-    b = x_arr.slice(( n / 2.to_f).ceil, n/2)
-    c = y_arr.slice(0, ( n / 2.to_f).ceil)
-    d = y_arr.slice(( n / 2.to_f).ceil, n/2)
+    n = [x_arr.length, y_arr.length].max
+    n2 = (n / 2.0).ceil
 
+    a, b = x_arr.each_slice(n2).to_a
+    c, d = y_arr.each_slice(n2).to_a
 
     ac = karatsuba(a.join.to_i, c.join.to_i)
     bd = karatsuba(b.join.to_i, d.join.to_i)
     ab_cd = karatsuba(a.join.to_i + b.join.to_i,
       c.join.to_i + d.join.to_i)
 
-
-    return ((10 ** n) * ac) +
+    return ((10 ** ((n/2)*2)) * ac) +
       ((10 ** (n/2)) * (ab_cd - ac - bd) ) +
       bd
   end
 end
 
+p karatsuba(10,10)
 p karatsuba(100,100)
+p karatsuba(250,250)
+p karatsuba(101,101)
+p karatsuba(1000,1000)
